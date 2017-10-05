fe3ff5292b4fcb18470e3d86f6fe201b60683916
rwart <32432397+rwart@users.noreply.github.com>
2017-10-05 21:03:01 +0200

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = 0, h = 0, triangleArea = 0 ;
+
+a = prompt('enter base a') ;
+h = prompt('enter height h') ;
+
+triangleArea = a*h/2 ;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
